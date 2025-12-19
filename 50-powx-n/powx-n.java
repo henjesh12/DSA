30471dd8fd65233d2ffec42bd2fbc99b1121c9fe
HENJESH K O <115684178+henjesh12@users.noreply.github.com>
2025-12-19 16:44:54 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (63.88%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,23 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(x ==0) return 0.0;
-        if(n == 1) return x;
-        if(n == 0)return 1.0;
-        if(x == 1)return 1.0;
-        if(x ==-1 && n%2 == 0)return 1.0;
-         if(x ==-1 && n%2 == 1)return -1.0;
-       long binForm = n;
-       if(binForm < 0){
-        x=(1/x);
-        binForm =-binForm;
-       }
-       double ans = 1;
-        while(binForm > 0){
-            if(binForm%2 ==1){
-                ans *= x;
-            }
-            x*=x;
-            binForm/=2;
-        }return ans;
-    }
+        long binform = n;
+        if(binform<0){
+            x=1/x;
+            binform = -binform;
+        }
+        double ans = 1;
+        while(binform > 0){
+        if(binform %2 == 1){
+            ans *=x;
+        }
+        
+        x*=x;
+        binform /=2;
+        }
+    return ans;
+     }
+   
 }
