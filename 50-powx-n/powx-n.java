491ab22716d1ba3e4154f7c159d98709b6290c5e
HENJESH K O <115684178+henjesh12@users.noreply.github.com>
2026-09-02 14:37:11 +0530

    Time: 1 ms (9.82%) | Memory: 48.2 MB (34.38%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,20 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        long binform = n;
-        if(binform<0){
-            x=1/x;
-            binform = -binform;
-        }
-        double ans = 1;
-        while(binform > 0){
-        if(binform %2 == 1){
-            ans *=x;
-        }
-        
-        x*=x;
-        binform /=2;
-        }
-    return ans;
+        long binForm = n;
+  double ans = 1;
+  if (binForm < 0) {
+       x = 1 / x;
+       binForm = -binForm;
+   }
+
+     while (binForm > 0){
+         if(binForm %2 ==1){
+             ans = ans*x;
+    }
+       x*=x;
+       binForm/=2;
      }
-   
+return ans;
+    }
 }
