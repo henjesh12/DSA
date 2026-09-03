7a1119d06fa2f5ae22bfc20345b75dd7f26f2397
HENJESH K O <115684178+henjesh12@users.noreply.github.com>
2026-09-03 22:16:59 +0530

    Time: 21 ms (41.88%) | Memory: 45.9 MB (46.73%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,30 +1,43 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
-        
-        for(int i=0;i<nums.length-3;i++){
-                  if(i>0 && nums[i] == nums[i-1]) continue;
+          List<List<Integer>> result = new ArrayList<>();
+            Arrays.sort(nums);
+           
+           for(int i=0;i<nums.length-3;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
             for(int j=i+1;j<nums.length-2;j++){
-                  if(j>i+1 && nums[j] == nums[j-1]) continue;
-            int k =j+1,l= nums.length-1;
-            while(k<l){
-                long sum =(long) nums[i] + nums[j]+ nums[k]+ nums[l];
+
+                if(j>i+1 && nums[j]==nums[j-1]){
+                    continue;
+                }
+              int left=j+1;
+              int right=nums.length-1;
+              while(left<right){
+                long sum = (long)nums[i]+nums[j]+nums[left]+nums[right];
                 if(sum == target){
-                    list.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                    while(k<l && nums[k] == nums[k+1])k++;
-                    while(k<l && nums[l] == nums[l-1])l--; 
-                    k++;
-                    l--;
+                    result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+
+                  while(left<right&&nums[left]==nums[left+1]){
+                    left++;
+                  }
+                  while(left<right&&nums[right]==nums[right-1]){
+                    right--;
+                  }
+
+                    left++;
+                    right--;
                 }else if(sum > target){
-                        l--;
-                      
-                }else {
-                    k++;
+                   right--;
+                }else{
+                    left++;
+                }
+              }
             }
-            }
-        }
+           }
+
+
+       return result;
     }
-    return list;
-}
 }
