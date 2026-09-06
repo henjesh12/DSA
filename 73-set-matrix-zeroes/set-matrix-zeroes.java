902f6e8ed671089c65c2e0d0eb7e67c56d7a9e4e
HENJESH K O <115684178+henjesh12@users.noreply.github.com>
2026-09-06 10:08:31 +0530

    Time: 1 ms (96.86%) | Memory: 47.6 MB (61.32%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -3,57 +3,30 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-       boolean firstRowZero = false;
-       boolean firstColumnZero = false;
-
-       for(int i=0;i<n;i++){
-           if(matrix[0][i] == 0){
-              firstRowZero = true;
-              break;
-           }
-       }
-
-       for(int j=0;j<m;j++){
-        if(matrix[j][0] == 0){
-            firstColumnZero = true;
-            break;
+        boolean row[] = new boolean[m];
+        boolean column[] = new boolean[n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    row[i]=true;
+                    column[j]=true;
+                }
+            }
         }
-       }
 
-       for(int i=1;i<m;i++){
-          for(int j=1;j<n;j++){
-            if(matrix[i][j] == 0){
-                matrix[i][0] = 0;
-                matrix[0][j] = 0;
+        for(int i=0;i<m;i++){
+            if(row[i]){
+                for(int j=0;j<n;j++){
+                    matrix[i][j]=0;
+                }
             }
-          }
-       }
- //zero rows
-       for(int i=1;i<m;i++){
-          if(matrix[i][0] == 0){
-            for(int j=1;j<n;j++){
-                matrix[i][j]=0;
-            }
-          }
-       }
- // zeros column
- for(int j=1;j<n;j++){
-    if(matrix[0][j]==0){
-        for(int i=1;i<m;i++){
-            matrix[i][j] = 0;
         }
-    }
- }
-           if(firstRowZero == true){
-            for(int j=0;j<n;j++) {
-                matrix[0][j]=0;
+        for(int j=0;j<n;j++){
+            if(column[j]){
+                for(int i=0;i<m;i++){
+                    matrix[i][j]=0;
+                }
             }
-           }
-
-           if(firstColumnZero == true) {
-            for(int i=0;i<m;i++){
-                matrix[i][0] = 0;
-            }
-           }
+        }
     }
 }
